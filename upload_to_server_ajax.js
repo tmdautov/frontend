22b834b250e1174c1a099eee04519c2d59b32f9f
Timur <tmdautov@gmail.com>
2016-04-06 08:21:26 +0600

    Update upload_to_server_ajax.js

--- upload_to_server_ajax.js
+++ upload_to_server_ajax.js
@@ -1,3 +1,10 @@
+
+/*
+    - Реализуйте js функцию, которая будет через ajax загружать данные на сервер
+    - функцию привязать к кнопке button
+    - данные будут приниматься с input=text
+*/
+
 function upload_data_to_server(var url, var data) {
     $.ajax({
        type: 'POST',
