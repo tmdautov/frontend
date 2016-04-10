32a26726ef570051d2e34b450cc77b7eaa575834
Timur <tmdautov@gmail.com>
2016-04-10 10:26:06 +0600

    Create generate_json.js

---
+++ generate_json.js
@@ -0,0 +1,18 @@
+
+
+// use json.parse to convert string to javascript object(json)
+
+
+function generate_json() {
+  var text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]
+  
+  }';
+  
+  var obj = JSON.parse(text);
+  
+  return obj; 
+  
+}
