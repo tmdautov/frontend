7c5d5e6023841ca063b6eebf3568e441257c3cd4
Timur <tmdautov@gmail.com>
2016-04-10 10:28:49 +0600

    Update generate_json.js

--- generate_json.js
+++ generate_json.js
@@ -4,12 +4,10 @@
 
 
 function generate_json() {
-  var text = '{ "employees" : 
-  [' +
-    '{ "firstName":"John" , "lastName":"Doe" },' +
-    '{ "firstName":"Anna" , "lastName":"Smith" },' +
-    '{ "firstName":"Peter" , "lastName":"Jones" }]
-  }';
+  var text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
   
   var obj = JSON.parse(text);
   
